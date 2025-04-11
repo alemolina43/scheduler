a440936dbf2023744bdc1f400342a5cf17fecb95
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-04-10 20:35:44 -0700

    Implement integration and E2E testing

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -12,14 +12,14 @@ export function formatSpots(number) {
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--full": props.spots === 0,
   });
 
   return (
     <li
+      data-testid="day"
       className={dayClass}
       onClick={() => props.setDay(props.name)}
-      data-testid="day"
     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
